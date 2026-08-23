9d7c6734512a82a68e76f47cf06fd52386566b81
rajashekhar_Reddy <shekharreddygangam@gmail.com>
2026-08-23 19:50:55 +0530

    script-files

---
+++ shel-script/03-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSON1=$1
+PERSON2=$2
+
+echo "$PERSON1: Hey $PERSON2, we are going to bomb you!"
+echo "$PERSON2: No, please don't bomb us!"
+echo "$PERSON1: We are going to bomb you seriously!"
+echo "$PERSON2: you are welcome to bomb us, but we will fight back!"
---
+++ shel-script/04-variables.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+echo "Please enter the user name:"
+read USERNAME # here USERNAME is the variable, what ever enter in terminal stores in this variable.
+echo "username is $USERNAME"
