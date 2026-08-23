384ce45643646972dda6cc84eac443dc64f8a296
rajashekhar_Reddy <shekharreddygangam@gmail.com>
2026-08-23 20:02:00 +0530

    script-files

--- shel-script/05-variables.sh
+++ shel-script/05-variables.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
-TIMESTAMP=$(date +%s)
+# TIMESTAMP=$(date +%s)
 
-echo "Time is: $TIMESTAMP "
+# echo "Time is: $TIMESTAMP "
 
+START_TIME=$(date +%s)
+ sleep 10
+
+ END_TIME=$(date +%s)
+
+ TOTAL_TIME=$(($END_TIME-$START_TIME))
+ echo "Total time executed: $TOTAL_TIME seconds"
