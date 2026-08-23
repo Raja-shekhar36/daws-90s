d9e1fd83bd11ca22ecb0e5b34c66a0767a20b1d8
rajashekhar_Reddy <shekharreddygangam@gmail.com>
2026-08-23 19:53:35 +0530

    script-files

--- shel-script/04-variables.sh
+++ shel-script/04-variables.sh
@@ -2,4 +2,10 @@
 
 echo "Please enter the user name:"
 read USERNAME # here USERNAME is the variable, what ever enter in terminal stores in this variable.
+
 echo "username is $USERNAME"
+
+echo "Please enter your password:"
+read -s PASSWORD # here PASSWORD is the variable, what ever enter in terminal stores in this variable.
+
+echo "password is $PASSWORD"
