938055be46e4cdc238fbb40acac8bfcc77c69151
rajashekhar_Reddy <shekharreddygangam@gmail.com>
2026-08-23 19:42:43 +0530

    script-files

--- shel-script/02-conversation.sh
+++ shel-script/02-conversation.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-echo "Trump: Hey Iran, we are going to bomb you!"
-echo "Iran: No, please don't bomb us!"
-echo "Trump: We are going to bomb you seriously!"
-echo "Iran: you are welcome to bomb us, but we will fight back!"
+PERSON1=Trump
+PERSON2=Iran
+
+echo "$PERSON1: Hey $PERSON2, we are going to bomb you!"
+echo "$PERSON2: No, please don't bomb us!"
+echo "$PERSON1: We are going to bomb you seriously!"
+echo "$PERSON2: you are welcome to bomb us, but we will fight back!"
