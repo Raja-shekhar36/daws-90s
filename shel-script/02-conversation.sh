f06a3010d1d1ae59a5949826573df4c2b3b0403d
rajashekhar_Reddy <shekharreddygangam@gmail.com>
2026-08-23 19:40:19 +0530

    script-files

---
+++ shel-script/02-conversation.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Trump: Hey Iran, we are going to bomb you!"
+echo "Iran: No, please don't bomb us!"
+echo "Trump: We are going to bomb you seriously!"
+echo "Iran: you are welcome to bomb us, but we will fight back!"
