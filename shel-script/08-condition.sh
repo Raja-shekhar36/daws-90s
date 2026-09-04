14375889024794c6eb951b1c441a08875055736b
rajashekhar_Reddy <shekharreddygangam@gmail.com>
2026-09-04 23:06:20 +0530

    condition-files

--- shel-script/08-condition.sh
+++ shel-script/08-condition.sh
@@ -12,7 +12,7 @@ NUMBER=$1
 if [ $NUMBER -ge 20 ]; then
     echo "Given number $NUMBER is greater than or equal to 20"
 elif [ $NUMBER -eq 20 ]; then
-    echo "Given number #NUMBER is equal to 20""
+    echo "Given number #NUMBER is equal to 20"
 else 
     echo "Given number $NUMBER is less than 20"
 fi
