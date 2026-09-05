774be39f14415e2357d814c66e819e2f55c177f0
rajashekhar_Reddy <shekharreddygangam@gmail.com>
2026-09-05 16:21:43 +0530

    condition-files

--- shel-script/10-logs.sh
+++ shel-script/10-logs.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
 USERID=$(id -u)
+LOGS_DIR=/home/ec2-user/daws-90s/shell-logs
+LOGS_FILE="$LOGS_DIR/$0.log" # /home/ec2-user/daws-90s/shell-logs/10-logs.sh.log
 
 # check root access or not
 if [ $USERID -ne 0 ]; then
     echo "Please run this script with root access"
     exit 1
 fi
+
 
 # echo "I am continuing...."
 
@@ -19,22 +22,22 @@ VALIDATE(){
     fi
 }
 
-dnf list installed mysql &>> $LOGS_FILE
+dnf list installed mysql &>>$LOGS_FILE
 
 if [ $? -eq 0 ]; then
     echo "MYSQL is already Installed ... SKIPPING"
 else
     echo "Installing MySQL"
-    dnf install mysql -y &>> $LOGS_FILE
+    dnf install mysql -y &>>$LOGS_FILE
     VALIDATE MySQL $?
 fi
 
-dnf list installed nginx &>> $LOGS_FILE
+dnf list installed nginx &>>$LOGS_FILE
 
 if [ $? -eq 0 ]; then
     echo "NGINX is already Installed ... SKIPPING"
 else
     echo "Installing NGINX"
-    dnf install nginx -y &>> $LOGS_FILE
+    dnf install nginx -y &>>$LOGS_FILE
     VALIDATE NGINX $?
 fi
