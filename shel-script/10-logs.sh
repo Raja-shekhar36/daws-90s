81be2f5ddd71b4f8b2a04c780651890892825ad6
rajashekhar_Reddy <shekharreddygangam@gmail.com>
2026-09-17 22:28:36 +0530

    log files added

--- shel-script/10-logs.sh
+++ shel-script/10-logs.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 USERID=$(id -u)
-LOGS_DIR=/home/ec2-user/daws-90s/shell-logs
+LOGS_DIR=/var/log/shell-script
 LOGS_FILE="$LOGS_DIR/$0.log" # /home/ec2-user/daws-90s/shell-logs/10-logs.sh.log
 
 # check root access or not
