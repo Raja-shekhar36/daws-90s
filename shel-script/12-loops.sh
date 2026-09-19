451af1824e96e2632c53019ae3511b93f5727bdc
rajashekhar_Reddy <shekharreddygangam@gmail.com>
2026-09-19 16:20:31 +0530

    loop files added

---
+++ shel-script/12-loops.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+USERID=$(id -u)
+LOGS_DIR=/var/log/shell-script
+LOGS_FILE="$LOGS_DIR/$0.log" # /var/log/shell-script/10-logs.sh.log
+
+
+# check root access or not
+if [ $USERID -ne 0 ]; then
+    echo "Please run this script with root access"
+    exit 1
+fi
+
+# echo "I am continuing...."
+
+VALIDATE(){
+    if [ $2 -ne 0 ];then
+        echo "Installing $1 is .....FAILED"
+        exit 1
+    else
+        echo "Installing $1 is .....SUCCESS"
+    fi
+}
+
+for package in $@
+do 
+    echo "Installing $package"
